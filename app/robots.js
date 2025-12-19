33078ab6ae96b89d7142964f5a65b291dc76e6be
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-12-19 10:14:01 +0530

    added

--- app/robots.js
+++ app/robots.js
@@ -8,28 +8,3 @@ export default function robots() {
     sitemap: 'https://thefullstack-dev.vercel.app/sitemap.xml',
   }
 }
-
-import Footer from "@/components/Footer/page";
-import Navbar from "@/components/Navbar/page";
-import { ThemeProvider } from "next-themes";
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
-          storageKey="theme"
-        >
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
